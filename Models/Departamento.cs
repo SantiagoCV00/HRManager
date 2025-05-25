8b4306a4975aa6d60729055028df62646649b379
Emerson <emerson@gmail.com>
2025-05-24 23:41:40 -0500

    Model Departamento

---
+++ Models/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRManager.Models
+{
+    public class Departamento
+    {
+        
+            [Key]
+            public int IdDepartamento { get; set; }
+
+            [Required]
+            public string NombreDepartamento { get; set; }
+
+            public string Ubicacion { get; set; }
+
+            public ICollection<Empleado> Empleados { get; set; }
+        
+    }
+}
