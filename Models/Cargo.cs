adaa4edea5fc98c7dc1620d0b06f91457da58a3a
Emerson <emerson@gmail.com>
2025-05-24 23:44:59 -0500

    Model Cargo

--- Models/Cargo.cs
+++ Models/Cargo.cs
@@ -1,6 +1,17 @@
-﻿namespace HRManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRManager.Models
 {
     public class Cargo
     {
+        [Key]
+        public int IdCargo { get; set; }
+
+        [Required]
+        public string TituloCargo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public ICollection<Empleado> Empleados { get; set; }
     }
 }
